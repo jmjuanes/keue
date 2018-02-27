7ccf65ea304a754bec6eb6d9be0c8f33d259d4cf
jmjuanes <josemijuanes@gmail.com>
2018-02-27 17:23:23 +0100

    Rewrite tests

--- test/test.js
+++ test/test.js
@@ -1,62 +1,58 @@
 //Import dependencies
-var assert = require('assert');
-var keue = require('../index.js');
+var assert = require("assert");
+var keue = require("../index.js");
 
-describe('keue', function()
-{
-  it('should execute all provided functions', function(done)
-  {
-    var executed1 = false;
-    var executed2 = false;
-    var k = keue(function(next)
-    {
-      executed1 = true;
-      return next(null);
+describe("keue", function () {
+    it("should execute all provided tasks", function (done) {
+        var executed1 = false;
+        var executed2 = false;
+        var k = keue();
+        k.addTask("task1", function (next) {
+            executed1 = true;
+            return next(null);
+        });
+        k.addTask("task2", function (next) {
+            executed2 = true;
+            setTimeout(function () {
+                return next();
+            }, 100);
+        });
+        k.run("task1", "task2");
+        k.on("finish", function () {
+            assert.equal(executed1, true);
+            assert.equal(executed2, true);
+            done();
+        });
     });
 
-    k.then(function(next)
-    {
-      executed2 = true;
-      setTimeout(function(){ return next(); }, 100);
-    });
+    it("should stop the queue when the keue.abort method is called", function (done) {
+        var executed1 = false, executed2 = false, executed3 = false;
+        var k = keue();
+        k.addTask("task1", function (next) {
+            executed1 = true;
+            return next();
+        });
 
-    k.finish(function(error)
-    {
-      assert.equal(error, null);
-      assert.equal(executed1, true);
-      assert.equal(executed2, true);
-      done();
-    });
-  });
+        k.addTask("task2", function (next) {
+            executed2 = true;
+            k.abort(new Error("Something went wrong"));
+            return next();
+        });
 
-  it('should stop the queue when a function returns an error', function(done)
-  {
-    var executed1 = false, executed2 = false, executed3 = false;
-    var k = keue(function(next)
-    {
-      executed1 = true;
-      return next();
+        k.addTask("task3", function (next) {
+            executed3 = true;
+            return done(new Error("Error"));
+        });
+        k.run("task1", "task2", "task3");
+        k.on("finish", function() {
+            return done(new Error("Error"));
+        });
+        k.on("abort", function (error) {
+            assert.notEqual(error, null);
+            assert.equal(executed1, true);
+            assert.equal(executed2, true);
+            assert.equal(executed3, false);
+            return done();
+        });
     });
-
-    k.then(function(next)
-    {
-      executed2 = true;
-      return next(new Error('Something went wrong'));
-    });
-
-    k.then(function(next)
-    {
-      executed3 = true;
-      return done(new Error('Error'));
-    });
-
-    k.finish(function(error)
-    {
-      assert.notEqual(error, null);
-      assert.equal(executed1, true);
-      assert.equal(executed2, true);
-      assert.equal(executed3, false);
-      return done();
-    });
-  });
 });
